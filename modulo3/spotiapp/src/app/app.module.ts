2555ea6578d186689a38a90c8280653cb1c03193
PLANFIAMX\gcasimiro <gcasimiro@planfia.com>
2019-01-10 17:32:32 -0600

    spotiapp

--- modulo3/spotiapp/src/app/app.module.ts
+++ modulo3/spotiapp/src/app/app.module.ts
@@ -2,21 +2,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
 import { HomeComponent } from './components/home/home.component';
 import { SearchComponent } from './components/search/search.component';
 import { ArtistComponent } from './components/artist/artist.component';
 import { SharedComponent } from './components/shared/shared.component';
+import { RouterModule } from '@angular/router';
+import { ROUTES } from './app.routes';
+
 
 @NgModule({
   declarations: [
     AppComponent,
+    NavbarComponent,
     HomeComponent,
     SearchComponent,
     ArtistComponent,
-    SharedComponent
+    SharedComponent,
+    
   ],
   imports: [
     BrowserModule
+    ,RouterModule.forRoot(ROUTES,{useHash:true})
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ modulo3/spotiapp/src/app/app.routes.ts
@@ -0,0 +1,14 @@
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { ArtistComponent } from './components/artist/artist.component';
+import { SearchComponent } from './components/search/search.component';
+
+
+
+export const ROUTES:Routes = [
+    { path: 'home', component: HomeComponent },
+    { path: 'artist', component: ArtistComponent },
+    { path: 'search', component: SearchComponent },    
+    { path: '**', pathMatch:'full', redirectTo: 'home' }
+];
+
