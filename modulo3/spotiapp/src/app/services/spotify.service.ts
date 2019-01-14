3bfde717f7e5f5c9d9b2101bf05ac18dea644828
PLANFIAMX\gcasimiro <gcasimiro@planfia.com>
2019-01-14 17:46:24 -0600

    Se agrega en una sola funcion el llamado a la API

--- modulo3/spotiapp/src/app/services/spotify.service.ts
+++ modulo3/spotiapp/src/app/services/spotify.service.ts
@@ -14,32 +14,49 @@ export class SpotifyService {
     
   }
 
+
+  getQuery(qry:string){
+  const url=`https://api.spotify.com/v1/${qry}` 
+
+  const headers =new HttpHeaders({
+    Authorization: "Bearer BQAyCIcvxvy8v5dh3sX3W3Kn6QD6NgcL7IWSIp-vFYPLmvnMVqnWSqXePyWhRZb7X7IgORXsNj1s4IJPhJA"
+  })
+
+  // console.log(url)
+    return this._peticionHttp.get(url,{headers})
+  }
   getNewReleases():any{
 
-    const headers =new HttpHeaders({
-      Authorization: "Bearer BQAyCIcvxvy8v5dh3sX3W3Kn6QD6NgcL7IWSIp-vFYPLmvnMVqnWSqXePyWhRZb7X7IgORXsNj1s4IJPhJA"
-    })
+    // const headers =new HttpHeaders({
+    //   Authorization: "Bearer BQAyCIcvxvy8v5dh3sX3W3Kn6QD6NgcL7IWSIp-vFYPLmvnMVqnWSqXePyWhRZb7X7IgORXsNj1s4IJPhJA"
+    // })
 
     // this._peticionHttp.get("https://api.spotify.com/v1/browse/new-releases",{headers})
     //   .subscribe((datos)=>{
     //     console.log(datos);
     //   })
 
-    return this._peticionHttp.get("https://api.spotify.com/v1/browse/new-releases",{headers})
-    .pipe(map((data) => {
-      return data['albums'].items
-    }));
+    return this.getQuery('browse/new-releases?limit=20')
+        .pipe(map((data) => {
+          return data['albums'].items
+      }));
+    // return this._peticionHttp.get(`https://api.spotify.com/v1/browse/new-releases`,{headers})
+    // .pipe(map((data) => {
+    //   return data['albums'].items
+    // }));
 
   }
 
   getArtists(terminos:string):any{
-    const headers =new HttpHeaders({
-      Authorization: "Bearer BQAyCIcvxvy8v5dh3sX3W3Kn6QD6NgcL7IWSIp-vFYPLmvnMVqnWSqXePyWhRZb7X7IgORXsNj1s4IJPhJA"
-    })
+    
 
-    return this._peticionHttp.get(`https://api.spotify.com/v1/search?q=${terminos}&type=track%2Cartist&limit=10&offset=5`,{headers})
-    .pipe(map((data) => {
+    return this.getQuery(`search?q=${terminos}&type=track%2Cartist&market=US&limit=10&offset=5`)
+        .pipe(map((data) => {
       return data['artists'].items
-    }));
+  }));
+    // return this._peticionHttp.get(`https://api.spotify.com/v1/browse/search?q=${terminos}&type=track%2Cartist&limit=10&offset=5`,{headers})
+    // .pipe(map((data) => {
+    //   return data['artists'].items
+    // }));
   }
 }
