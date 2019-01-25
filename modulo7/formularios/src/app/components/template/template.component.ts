cec7db9399918e6462d181b659c382930c13c1bd
PLANFIAMX\gcasimiro <gcasimiro@planfia.com>
2019-01-25 10:52:08 -0600

    Se agrega un formulario y como manejar por template los datos con  valores por default tanto si modifican un objeto origen como si no

--- modulo7/formularios/src/app/components/template/template.component.ts
+++ modulo7/formularios/src/app/components/template/template.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { log } from 'util';
 
 @Component({
   selector: 'app-template',
@@ -7,9 +9,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TemplateComponent implements OnInit {
 
+
+  usuariodefault1:Object={
+    nombre:"nombre default"
+    ,apellido:"apellido default"
+    ,email:"default@default.com"
+  }
+
+  usuariodefault2:Object={
+    nombre:"nombre default"
+    ,apellido:"apellido default"
+    ,email:"default@default.com"
+  }
   constructor() { }
 
   ngOnInit() {
   }
 
+  guardar(forma:NgForm){
+    console.log("Datos de ngForm",forma);        
+    console.log("Valor campos:",forma.value);
+    console.log("Obejto original:",this.usuariodefault1);        
+    
+  }
+
+  guardar2(forma:NgForm){
+    console.log("Datos de ngForm",forma);        
+    console.log("Valor campos:",forma.value);
+    console.log("Obejto original:",this.usuariodefault1);        
+    
+  }
+  guardar3(forma:NgForm){
+    console.log("Datos de ngForm",forma);        
+    console.log("Valor campos:",forma.value);
+    console.log("Obejto original:",this.usuariodefault2);        
+    
+  }
 }
