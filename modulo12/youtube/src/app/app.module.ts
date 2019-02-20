c563065f3c215bbd1a223594eeda5b342b91448a
PLANFIAMX\gcasimiro <gcasimiro@planfia.com>
2019-02-20 17:27:26 -0600

    Se inicia la app de youtube

---
+++ modulo12/youtube/src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+//Componentes
+import { HomeComponent } from './components/home/home.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+
+//Servicios
+import { YoutubeService } from './services/youtube.service';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    NavbarComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [YoutubeService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ modulo12/youtube/src/app/services/youtube.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class YoutubeService {
+
+   api:string="AIzaSyBjROwCgXNVKB38HikHx2WfkaQv17k9HhQ"
+
+  constructor() { }
+}
